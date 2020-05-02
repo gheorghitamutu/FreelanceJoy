7de26dd077d429bf2e626f9a61645826be039d6c
Gheorghita Mutu <gmutu@bitdefender.com>
2020-05-02 05:21:43 +0300

    ^ changing auth (WIP)
    Signed-off-by: Gheorghita Mutu <gmutu@bitdefender.com>

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # https://googleapis.dev/python/datastore/latest/index.html
+# https://cloud.google.com/community/tutorials/secrets-manager-python
 
 import datetime
 import logging
 import os
 
+import google.oauth2.credentials
 import google.oauth2.id_token
-from flask import Flask, render_template, request
+import pyrebase
+from flask import Flask, render_template, request, json, redirect
 from google.auth.transport import requests
-from google.cloud import datastore
+from google.cloud import datastore, secretmanager
+from google_auth_oauthlib.flow import Flow
 
 
 class App(Flask):
@@ -19,8 +23,31 @@ class App(Flask):
 
         self.data_store_client = datastore.Client()
         self.firebase_request_adapter = requests.Request()
+        self.secrets = secretmanager.SecretManagerServiceClient()
+        self.firebase_api_key = \
+            self.secrets.access_secret_version("projects/927858267242/secrets/FIREBASE_API_KEY/versions/1") \
+                .payload.data.decode("utf-8")
+        self.client_secret = \
+            json.loads(self.secrets.access_secret_version("projects/927858267242/secrets/CLIENT_SECRET/versions/1")
+                       .payload.data.decode("utf-8"))
+
+        self.firebase_config = {
+            'apiKey': self.firebase_api_key,
+            'authDomain': 'freelancejoy.firebaseapp.com',
+            'databaseURL': 'https://freelancejoy.firebaseio.com',
+            'projectId': 'freelancejoy',
+            'storageBucket': 'freelancejoy.appspot.com',
+            'messagingSenderId': '927858267242',
+            'appId': '1:927858267242:web:7e0e0e8251af5d80ef2613',
+            'measurementId': 'G-GZE64K9308'
+        }
+        self.firebase = pyrebase.initialize_app(self.firebase_config)
+        self.firebase_auth = self.firebase.auth()
+        self.firebase_db = self.firebase.database()
 
         self.add_url_rule('/', view_func=self.root, methods=['GET', 'POST'])
+        self.add_url_rule('/login', view_func=self.login, methods=['GET', 'POST'])
+        self.add_url_rule('/__/auth/handler/?<path:confirmation>', view_func=self.handle_auth, methods=['GET', 'POST'])
         self.register_error_handler(500, self.server_error)
 
     def store_time(self, email, dt):
@@ -66,9 +93,39 @@ class App(Flask):
 
         return render_template(
             'index.html',
-            user_data=claims, error_message=error_message, times=times)
+            user_data=claims, error_message=error_message, times=times, firebase_config=self.firebase_config)
 
     # [END gae_python37_auth_verify_token]
+
+    def login(self):
+        # Use the client_secret.json file to identify the application requesting
+        # authorization. The client ID (from that file) and access scopes are required.
+        flow = Flow.from_client_config(
+            self.client_secret,
+            scopes=['profile', 'email'])
+
+        # Indicate where the API server will redirect the user after the user completes
+        # the authorization flow. The redirect URI is required. The value must exactly
+        # match one of the authorized redirect URIs for the OAuth 2.0 client, which you
+        # configured in the API Console. If this value doesn't match an authorized URI,
+        # you will get a 'redirect_uri_mismatch' error.
+        flow.redirect_uri = 'https://freelancejoy.firebaseapp.com/__/auth/handler'
+
+        # Generate URL for request to Google's OAuth 2.0 server.
+        # Use kwargs to set optional request parameters.
+        authorization_url, state = flow.authorization_url(
+            # Enable offline access so that you can refresh an access token without
+            # re-prompting the user for permission. Recommended for web server apps.
+            access_type='offline',
+            # Enable incremental authorization. Recommended as a best practice.
+            include_granted_scopes='true')
+
+        return redirect(authorization_url)
+
+    @staticmethod
+    def handle_auth(confirmation):
+        print(request.get('https://www.googleapis.com/userinfo/v2/me').json())
+        return confirmation
 
     @staticmethod
     def server_error(e):
--- web/static/js/handle_auth.js
+++ web/static/js/handle_auth.js
@@ -1,5 +1,20 @@
 'use strict';
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBGSIJAtDKQWCW0_5CF1sf1xdVxPE_qwlY",
+    authDomain: "freelancejoy.firebaseapp.com",
+    databaseURL: "https://freelancejoy.firebaseio.com",
+    projectId: "freelancejoy",
+    storageBucket: "freelancejoy.appspot.com",
+    messagingSenderId: "927858267242",
+    appId: "1:927858267242:web:7e0e0e8251af5d80ef2613",
+    measurementId: "G-GZE64K9308"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 // [START gae_python37_auth_javascript]
 window.addEventListener('load', function () {
   document.getElementById('sign-out').onclick = function () {
