ce56a89654b708eb6dec4a668619e2ac9f0abeff
Gheorghita Mutu <gmutu@bitdefender.com>
2020-05-04 11:29:04 +0300

    - removed phone custom handlers
    Signed-off-by: Gheorghita Mutu <gmutu@bitdefender.com>

--- web/static/js/handle_auth.js
+++ web/static/js/handle_auth.js
@@ -101,27 +101,5 @@ window.addEventListener('load', function() {
         console.log(error);
         alert('Unable to log in: ' + error)
     });
-
-    var phoneNumber = getPhoneNumberFromUserInput();
-    var appVerifier = window.recaptchaVerifier;
-    firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
-        .then(function(confirmationResult) {
-            // SMS sent. Prompt user to type the code from the message, then sign the
-            // user in with confirmationResult.confirm(code).
-            window.confirmationResult = confirmationResult;
-        }).catch(function(error) {
-            // Error; SMS not sent
-            // ...
-        });
-
-    var code = getCodeFromUserInput();
-    confirmationResult.confirm(code).then(function(result) {
-        // User signed in successfully.
-        var user = result.user;
-        // ...
-    }).catch(function(error) {
-        // User couldn't sign in (bad verification code?)
-        // ...
-    });
 });
 // [END gae_python37_auth_javascript]
