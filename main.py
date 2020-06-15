5c413f95f387c47bbfda907b8c672a6ddebad562
Cernat Iulian <iulian.cernat98@yahoo.com>
2020-06-15 22:23:15 +0300

    # fixed bug with current marketplace category

--- main.py
+++ main.py
@@ -399,12 +399,12 @@ class App(Flask):
         page = request.args.get('page', 1, type=int)
         per_page = request.args.get('per_page', 10, type=int)
         category_id = request.args.get('category_id', categories_list[0]['id'], type=int)
-        category_name= request.args.get('category_name', categories_list[0]['name'], type=int)
+        category_name = request.args.get('category_name', categories_list[0]['name'], type=str)
         product_list = self.get_products_by_category(url_root, page, per_page, category_id)
 
-
         return render_template('marketplace.html', session=self.session, products=product_list,
-                               categories=categories_list, current_category_id=category_id, current_category_name=category_name)
+                               categories=categories_list, current_category_id=category_id,
+                               current_category_name=category_name)
 
     def product(self, product_id):
         url_root = request.url_root
